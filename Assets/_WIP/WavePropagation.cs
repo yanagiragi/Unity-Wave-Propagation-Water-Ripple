3b4fac8344183ba754450accb4164a01c7975072
yanagiragi <qaz90473@gmail.com>
2018-02-02 12:53:59 +0800

    Update yanagi's version

---
+++ Assets/_WIP/WavePropagation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WavePropagation : PostEffectsBase {
+
+    public Shader Shader;
+    private Material m_Material;
+
+    [Range(0.0f, 0.3f)]
+    public float RippleRadius = 0.05f;
+
+    public Material material
+    {
+        get
+        {
+            m_Material = CheckShaderAndCreateMaterial(Shader, m_Material);
+            return m_Material;
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material)
+        {
+            material.SetFloat("_RippleRadius", RippleRadius);
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
